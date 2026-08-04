1f3b710d2e6c80d2e96ad1d4bc54bc22b45b8f65
nexths <henriquenik@gmail.com>
2026-08-03 21:47:52 -0300

    Tour virtual

---
+++ Tour Aero Pousada Gru/data.js
@@ -0,0 +1,219 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-area-externa",
+      "name": "Area externa",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": -2.953110941403313,
+        "pitch": -0.07715838491066407,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.05702284317282924,
+          "pitch": 0.25216630899642034,
+          "rotation": 0,
+          "target": "4-quarto-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-quarto-4",
+      "name": "Quarto 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": -0.6625052360001469,
+        "pitch": 0.07393630299842258,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0344568458542884,
+          "pitch": 0.1709562357947565,
+          "rotation": 0,
+          "target": "2-quarto-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-quarto-3",
+      "name": "Quarto 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": 0.6458201424053218,
+        "pitch": 0.05658913571436308,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.392330949765171,
+          "pitch": 0.25254864683270206,
+          "rotation": 1.5707963267948966,
+          "target": "3-quarto-2"
+        },
+        {
+          "yaw": -2.5755264717113526,
+          "pitch": 0.23626513551377037,
+          "rotation": 4.71238898038469,
+          "target": "1-quarto-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-quarto-2",
+      "name": "Quarto 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": 0.4573680654074117,
+        "pitch": 0.12006816018186228,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.153785500238298,
+          "pitch": 0.18515812474395688,
+          "rotation": 1.5707963267948966,
+          "target": "4-quarto-1"
+        },
+        {
+          "yaw": -2.3861692968481663,
+          "pitch": 0.18476420953456163,
+          "rotation": 4.71238898038469,
+          "target": "2-quarto-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-quarto-1",
+      "name": "Quarto 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": 0.6367691985979285,
+        "pitch": 0.1278500192146268,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.100294446721273,
+          "pitch": 0.24510713796473382,
+          "rotation": 1.5707963267948966,
+          "target": "0-area-externa"
+        },
+        {
+          "yaw": -2.337511554889627,
+          "pitch": 0.23285592518616838,
+          "rotation": 4.71238898038469,
+          "target": "3-quarto-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
